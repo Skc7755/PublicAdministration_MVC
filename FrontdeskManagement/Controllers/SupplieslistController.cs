230eff1e45fce161e982cb058fb1affa71604315
郑世纪 <@AGONI>
2020-10-24 17:07:00 +0800

    郑世纪

---
+++ FrontdeskManagement/Controllers/SupplieslistController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontdeskManagement.Controllers
+{
+    /// <summary>
+    /// 用户列表
+    /// </summary>
+    public class SupplieslistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 用品列表
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult List()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 用品登记
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Registers()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 用品详情
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Particulars()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FrontdeskManagement/Controllers/SuppliesprocurementController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontdeskManagement.Controllers
+{
+    /// <summary>
+    /// 用品采购
+    /// </summary>
+    public class SuppliesprocurementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 用品采购列表
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult List()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 用品采购登记
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Registers()
+        {
+            
+            return View();
+        }
+        /// <summary>
+        /// 用品采购详情
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Particulars()
+        {
+            return View();
+        }
+    }
+}
