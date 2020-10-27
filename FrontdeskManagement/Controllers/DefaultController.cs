0c901df0c0502b78ce7c636166216ce7dd6f2db9
帅康 程 <2814543600@qq.com>
2020-10-27 16:01:26 +0800

    删改

--- FrontdeskManagement/Controllers/DefaultController.cs
+++ FrontdeskManagement/Controllers/DefaultController.cs
@@ -16,5 +16,118 @@ namespace FrontdeskManagement.Controllers
         {
             return View();
         }
+        public IActionResult VehicleInfoList()
+        {
+            return View();
+        }
+        public IActionResult AddCar()
+        {
+            return View();
+              
+        }
+        public IActionResult CarDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+        public IActionResult CarApplicationList()
+        {
+            return View();
+        }
+        public IActionResult CarApplicationDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+        public IActionResult AIRList()
+        {
+            return View();
+        }
+        public IActionResult AIRDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+
+        }
+        public IActionResult VMRList()
+        {
+            return View();
+        }
+        public IActionResult VMRDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+
+        }
+        public IActionResult AIROVList()
+        {
+            return View();
+        }
+        public IActionResult AIROVDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+
+        }
+        public IActionResult VRRList()
+        {
+            return View();
+        }
+        public IActionResult VRRDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+
+        }
+        public IActionResult VARList()
+        {
+            return View();
+        }
+        public IActionResult VARDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+
+        }
+        public IActionResult AssetsList()
+        {
+            return View();
+        }
+        public IActionResult AssetsListDetail(int id)
+        {
+
+            ViewData["id"] = id;
+            return View();
+            
+        }
+        public IActionResult CFAList()
+        {
+            return View();
+        }
+        public IActionResult CFAListDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        
+        }
+        public IActionResult AfarList()
+        {
+            return View();
+        
+        }
+        public IActionResult AfarListDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+        public IActionResult APAList()
+        {
+            return View();
+        }
+        public IActionResult APAListDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
     }
 }
